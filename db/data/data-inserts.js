de88f122c94b7d241a094188ae25d60f0a03be20
Priscilla <priscillavitorino1@gmail.com>
2025-05-29 17:46:41 +0100

    GET properties

--- db/data/data-inserts.js
+++ db/data/data-inserts.js
@@ -1,8 +1,9 @@
 const db = require("./connection")
 const format = require("pg-format")
 
-function insertPropertyType (){
+function insertPropertyType (propertyTypesData){
     return db.query(
+        
         format(`INSERT INTO property_types (property_type, description)
                 VALUES %L RETURNING *;`,
                 propertyTypesData.map(({property_type, description}) => [property_type, description]))
@@ -10,6 +11,71 @@ function insertPropertyType (){
 );
 }
 
+function insertUsers (formatedUsers){
+    return db.query(
+        format(`INSERT INTO users (first_name, surname, email, phone_number, avatar, is_host)
+                VALUES %L RETURNING *;`, formatedUsers)
+)
+}
 
+function insertProperties (formatedProperties){
+    return db.query(
+        format(`
+        INSERT INTO properties (name, property_type, location, price_per_night, description, host_id)
+        VALUES %L RETURNING *;
+        `, formatedProperties)
+)
+}
 
-module.exports = insertPropertyType
+function insertAmenities (formatedAmenities){
+    return db.query(
+        format(`INSERT INTO amenities (amenity) VALUES %L RETURNING *;`, formatedAmenities.map((item)=>[item]))
+)
+}
+
+function insertPropertiesAmenities(formatedPropertiesAmenities){
+    return db.query(format(`
+        INSERT INTO properties_amenities (property_id, amenity_slug)
+        VALUES %L;`, formatedPropertiesAmenities)
+)
+}
+
+function insertImages(formatedImages){
+    return db.query(
+        format(`INSERT INTO images (property_id, image_url, alt_text)
+                VALUES %L RETURNING *;`, formatedImages)
+)
+}
+
+function insertFavourites(formatedFavourites){
+    return db.query(
+        format(`INSERT INTO favourites (guest_id, property_id)
+                VALUES %L RETURNING *;`, formatedFavourites)
+        )
+}
+
+function insertReviews(formatedReviews){
+    return db.query(
+        format(`INSERT INTO reviews (property_id, guest_id, rating, comments)
+                VALUES %L;`, formatedReviews)
+)
+}
+
+function insertBookings(formatedBookings){
+    return db.query(
+        format(`INSERT INTO bookings(property_id, guest_id, check_in_date, check_out_date)
+                VALUES %L;`, formatedBookings)
+)
+}
+
+module.exports = {
+    insertPropertyType, 
+    insertUsers, 
+    insertProperties,
+    insertAmenities,
+    insertPropertiesAmenities,
+    insertImages,
+    insertFavourites,
+    insertReviews,
+    insertBookings
+}
--- db/data/seed.js
+++ db/data/seed.js
@@ -1,5 +1,3 @@
-const db = require("./connection")
-const format = require("pg-format")
 const manageTables = require("./manage-tables.js")
 const {
         formatUsers, 
@@ -12,65 +10,47 @@ const {
         formatBookings
 } = require("../utils/format.js")
 
+const {
+        insertPropertyType, 
+        insertUsers,
+        insertProperties,
+        insertAmenities,
+        insertPropertiesAmenities,
+        insertImages,
+        insertFavourites,
+        insertReviews,
+        insertBookings
+} = require("./data-inserts.js")
+
 
 async function seed(propertyTypesData, usersData, propertiesData, imagesData, favouritesData, reviewsData, bookingsData){
     await manageTables();
     
-        await db.query(
-                        format(`INSERT INTO property_types (property_type, description)
-                                VALUES %L RETURNING *;`,
-                                propertyTypesData.map(({property_type, description}) => [property_type, description]))
-                                
-        );
-
-        const formatedUsers = formatUsers(usersData)
-        const {rows: insertedUsers} = await db.query(
-                        format(`INSERT INTO users (first_name, surname, email, phone_number, avatar, is_host)
-                                VALUES %L RETURNING *;`, formatedUsers)
-        )
-
-        const formatedProperties = formatProperties(propertiesData, insertedUsers)
-        const {rows: insertedProperties} = await db.query(
-                format(`
-                INSERT INTO properties (name, property_type, location, price_per_night, description, host_id)
-                VALUES %L RETURNING *;
-                `, formatedProperties)
-        )
-
-        const formatedAmenities = formatAmenities(propertiesData)
-        await db.query(
-                        format(`INSERT INTO amenities (amenity) VALUES %L RETURNING *;`, formatedAmenities.map((item)=>[item]))
-        )
-
-        const formatedPropertiesAmenities = formatPropertiesAmenities(propertiesData, insertedUsers, insertedProperties);
-        await db.query(format(`
-                                INSERT INTO properties_amenities (property_id, amenity_slug)
-                                VALUES %L;`, formatedPropertiesAmenities)
-        )
-
-        const formatedImages = formatImages(imagesData, insertedProperties)
-        await db.query(
-        format(`INSERT INTO images (property_id, image_url, alt_text)
-                VALUES %L RETURNING *;`, formatedImages)
-                )
-
-        const formatedFavourites = formatFavourites(favouritesData, insertedUsers, insertedProperties)
-        await db.query(
-                format(`INSERT INTO favourites (guest_id, property_id)
-                        VALUES %L RETURNING *;`, formatedFavourites)
-                )
-
-        const formatedReviews = formatReviews(insertedProperties, insertedUsers, reviewsData)
-        await db.query(
-                format(`INSERT INTO reviews (property_id, guest_id, rating, comments)
-                        VALUES %L;`, formatedReviews)
-        )
-
-        const formatedBookings = formatBookings(insertedProperties, insertedUsers, bookingsData)
-        await db.query(
-                format(`INSERT INTO bookings(property_id, guest_id, check_in_date, check_out_date)
-                        VALUES %L;`, formatedBookings)
-        )
+    const { rows: insertedPropertyType } = await insertPropertyType(propertyTypesData)
+    
+    const formatedUsers = formatUsers(usersData)
+    const {rows: insertedUsers} = await insertUsers(formatedUsers)
+    
+    const formatedProperties = formatProperties(propertiesData, insertedUsers)
+    const {rows: insertedProperties} = await insertProperties(formatedProperties)
+    
+    const formatedAmenities = formatAmenities(propertiesData)
+    await insertAmenities(formatedAmenities)
+    
+    const formatedPropertiesAmenities = formatPropertiesAmenities(propertiesData, insertedUsers, insertedProperties);
+    await insertPropertiesAmenities(formatedPropertiesAmenities)
+    
+    const formatedImages = formatImages(imagesData, insertedProperties)
+    await insertImages(formatedImages)
+    
+    const formatedFavourites = formatFavourites(favouritesData, insertedUsers, insertedProperties)
+    await insertFavourites(formatedFavourites)
+    
+    const formatedReviews = formatReviews(insertedProperties, insertedUsers, reviewsData)
+    await insertReviews(formatedReviews)
+    
+    const formatedBookings = formatBookings(insertedProperties, insertedUsers, bookingsData)
+    await insertBookings(formatedBookings)
 }
 
 module.exports = seed;
